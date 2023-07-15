9c55787d3842e3b957e4fbafe93e1cf1a6592ca4
saanspeur <evelin.ortega018@gmail.com>
2023-07-14 17:46:17 -0700

    added functions for add department/employee/role

--- index.js
+++ index.js
@@ -76,3 +76,148 @@ function viewEmployees() {
   });
 }
 
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the name of the department you would like to create?",
+        name: "name",
+      },
+    ])
+    .then((answers) => {
+      db.query("INSERT INTO department SET ?", answers, function (err) {
+        if (err) throw err;
+        console.log("Department Created");
+        setTimeout(mainQuestion, 2000);
+      });
+    });
+}
+
+function addRole() {
+  db.query("SELECT * FROM department", function (err, results) {
+    const departments = results.map((department) => ({
+      name: department.name,
+      value: department.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the name of the role you would like to create?",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "What is the salary for this role?",
+          name: "salary",
+        },
+        {
+          type: "list",
+          message: "What department does this role belong to?",
+          name: "department_id",
+          choices: departments,
+        },
+      ])
+      .then((answers) => {
+        db.query("INSERT INTO role SET ?", answers, function (err) {
+          if (err) throw err;
+          console.log("Role Created");
+          setTimeout(mainQuestion, 2000);
+        });
+      });
+  });
+}
+
+function addEmployee() {
+  db.query("SELECT * FROM role", function (err, results) {
+    const roles = results.map((role) => ({
+      name: role.title,
+      value: role.id,
+    }));
+    db.query("SELECT * FROM employee", function (err, results) {
+      const employees = results.map((employee) => ({
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      }));
+      inquirer
+        .prompt([
+          {
+            type: "input",
+            message: "What is the first name of the employee you want to add?",
+            name: "first_name",
+          },
+          {
+            type: "input",
+            message: "What is the last name of the employee you want to add?",
+            name: "last_name",
+          },
+          {
+            type: "list",
+            message: "What role does this employee have?",
+            name: "role_id",
+            choices: roles,
+          },
+          {
+            type: "list",
+            message: "What manager does this employee have?",
+            name: "manager_id",
+            choices: employees,
+          },
+        ])
+        .then((answers) => {
+          db.query("INSERT INTO employee SET ?", answers, function (err) {
+            if (err) throw err;
+            console.log("Employee Created");
+            setTimeout(mainQuestion, 2000);
+          });
+        });
+    });
+  });
+}
+
+function updateEmployee() {
+  db.query("SELECT * FROM role", function (err, results) {
+    const roles = results.map((role) => ({
+      name: role.title,
+      value: role.id,
+    }));
+    db.query("SELECT * FROM employee", function (err, results) {
+      const employees = results.map((employee) => ({
+        name: employee.first_name + " " + employee.last_name,
+        value: employee.id,
+      }));
+
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            message: "What employee do you want to update?",
+            name: "employee_id",
+            choices: employees,
+          },
+          {
+            type: "list",
+            message: "What is this employees new role?",
+            name: "role_id",
+            choices: roles,
+          },
+        ])
+        .then((answers) => {
+          db.query(
+            "UPDATE employee SET role_id = ? WHERE id = ?",
+            answers,
+            function (err) {
+              if (err) throw err;
+              console.log("Employee Updated");
+              setTimeout(mainQuestion, 2000);
+            }
+          );
+          db.query("UPDATE employee SET role_id = ? WHERE id = ?", [
+            roleId,
+            employeeId,
+          ]);
+        });
+    });
+  });
+}
